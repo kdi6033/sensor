f5cba5c6715e259009831ae4a148a3e25fe7ad95
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2018-09-12 11:32:31 +0900

    온도습도 웹페이지 연결
    ipTime 무선중계기는 DHCP를 사용으로 설정한다. 유튜브에서 설명한 것과 같이 D1 mini는 공유기로부터 IP를 할당 받고 자체적으로 웹페이지 서비스를 한다. 단순 웹페이지는 이용자가 접속을 할 때만 웹페이지가 업로드되어 새로운 온도 변화를 모니터링하기 위해서는 일정시간 간격으로 웹페이지가 새로 쓰여지도록 했다. 사진은 자동으로 온도와 습도를 웹브라우져에서 모니터링한다.

--- 3-3-2 온습도센서(MHTP-485S) 웹페이지/TempWep01/TempWep01.ino
+++ 3-3-2 온습도센서(MHTP-485S) 웹페이지/TempWep01/TempWep01.ino
@@ -1,7 +1,3 @@
-/* D3(GPIO0,Pull-up) 핀이 Max485의 RE DE 핀에 연결되어 있다. - 쓰기모드
- * D3 = LOW 일때 읽기모드이다.
- */
-
 #include <ESP8266WiFi.h>
 
 const char* ssid = "i2r";
@@ -12,25 +8,19 @@ String webString = "";
 String commandString = ":80040004000276";
 boolean stringComplete = false;  // whether the string is complete
 
-const int contPin =  0;
-
 // Create an instance of the server
 // specify the port to listen on as an argument
 WiFiServer server(80);
 
 void setup() {
-  pinMode(contPin, OUTPUT);
-  digitalWrite(contPin, LOW); // RS485 읽기 모드 
-  //digitalWrite(contPin, HIGH); // RS485 쓰기 모드 
-  
   // initialize serial:
   Serial.begin(9600);
+  Serial1.begin(9600);
   delay(10);
   // 온습도 요청 명령문 CR(13), LF(10) 추가 이보드는 자동으로 붙는다.
   //commandString += char(13);
   //commandString += char(10);
   //commandString += char(0);
-
   
   // Connect to WiFi network
   Serial.println();
@@ -77,19 +67,31 @@ void loop() {
   // Read the first line of the request
   String req = client.readStringUntil('\r');
   Serial.println(req);
+  
   client.flush();
-
   // Prepare the response
-          client.println("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n");
-          client.println("<body>");
-          client.println("<br />");
-          client.println(webString);
-          client.println("</body>");
-          client.println("</html>");
-
+  client.println("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n");
+  //정기적으로 페이지 업데이트
+  client.println("<head>\r\n<meta http-equiv='refresh' content='5'/>\r\n");
+  client.println("<title>Humidity & Teperature</title>\r\n<style>\r\n");
+  client.println("body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\r\n");
+  client.println("</style>\r\n</head>\r\n");
+  client.println("<body>");
+  client.println("<br />");
+  client.println("<h1>");
+  // 온도와 습도 표시
+  client.println(webString);
+  client.println("</h1>");
+  client.println("</body>");
+  client.println("</html>");  
+  
   delay(1);
   Serial.println("Client disonnected");
   
+}
+
+void requestTX() {
+  Serial1.println(commandString);
 }
 
 // 시리얼통신으로 문자가 들어오면 처리한다.
@@ -98,37 +100,32 @@ void serialEvent() {
     size_t len = Serial.available();
     uint8_t sbuf[len];
     Serial.readBytes(sbuf, len);
-    inputString="";
+    String inputString="";
     for(int i=0;i<len;i++)
       inputString += char(sbuf[i]);
     Serial.println(inputString);
-  }
-}
-
-/*
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // LF (아스키번호 10)이 들어오면 하나의 문장처리
-    if (inChar == char(10)) {
-      stringComplete = true;
+    if( inputString.startsWith(":800404") == true) {
+      int Humi, Temp;
+      Serial.println("OK");
+      Humi = HexToInt(sbuf[7])*16*16*16 + HexToInt(sbuf[8])*16*16 + 
+      HexToInt(sbuf[9])*16 + HexToInt(sbuf[10]);
+      Serial.println((float)Humi/100);
+      Temp = HexToInt(sbuf[11])*16*16*16 + HexToInt(sbuf[12])*16*16 + 
+      HexToInt(sbuf[13])*16 + HexToInt(sbuf[14]);
+      Serial.println((float)Temp/100);
+      webString = " Humidity: " + String((float)Humi/100) + "<br /> Temperature: " + String((float)Temp/100);
     }
-    Serial.println(Serial.available());
-    Serial.println(inChar);
   }
 }
-*/
 
-void requestTX() {
-  digitalWrite(contPin, HIGH);
-  delay(1000);
-  Serial.println(commandString);
-  delay(100);
-  digitalWrite(contPin, LOW);
-  delay(100);
+int HexToInt(char hex) {
+  if( hex <= '9')
+    return hex - '0';
+  else if (hex <= 'F')
+    return hex-'A' +10;
+  else if (hex <= 'f')
+    return hex-'a' +10;
 }
+
 
 
