c06664bbe66cc11bbd7918dc590ed412d0775e53
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2018-11-10 21:44:41 +0900

    자동 웹갱신, 한글 웹, mqtt 없음
    한글웹이 자동으로 갱신되어 데이타를 업로드 한다. mqtt는 연결 않했음

--- 3-3-5_온습도센서(MHTP-485S) MQTT 한글/3-3-5_mhtp485/3-3-5_mhtp485.ino
+++ 3-3-5_온습도센서(MHTP-485S) MQTT 한글/3-3-5_mhtp485/3-3-5_mhtp485.ino
@@ -8,11 +8,21 @@
 
 MDNSResponder mdns;
 
+/*
+ * 공유기 설정에서 i2r_ap_***** 로 되어있는 ap 공유기를 선택한다. 비밀번호는 00000000 이다.
+ * 공유기를 입력하기 위해 크롬의 주소창에 "i2r.local"을 입력하여 공유기 설정을 선택한다.
+ * 또는 http://i2r.local/wifi  http://192.168.4.1 을 입력해도 된다.
+ * 입력 후 i2r.local 의 메인 페이지에 입력한 공유기의 이름과 주소가 정상적으로 출력되면
+ * 공유기 주소를 선택해 인테넷이 연결될 서버의 주소를 선택한 후 
+ * 설정에서 입력한 공유기를 선택하면 정상적으로 사용 할 수 있다.
+ */
+ 
 /* Set these to your desired softAP credentials. They are not configurable at runtime */
 const char *softAP_ssid = "i2r_ap_";
 const char *softAP_password = "00000000";
 String sAP_ssid;
 char cAP_ssid[20];
+int setAP=1;// 1 이면 ap 설정중 으로 메인 페이지에 주소 표시
 
 /* hostname for mDNS. Should work at least on windows. Try http://i2r.local */
 const char *myHostname = "i2r";
@@ -46,7 +56,7 @@ unsigned int status = WL_IDLE_STATUS;
 String inputString = "";         // 수신 데이타 저장
 boolean stringComplete = false;  // 수신 완료되면 true
 String s;  // 보내는 문자열
-int count,j;
+int count,j,serialj=0;
 long lastMsg = 0;
 int delayTime=4000; //mqtt로 메세지 보낸는 시간 (단위 ms)
 String commandString = ":80040004000276";
@@ -105,10 +115,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  /*
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
+  */
 }
 
 void reconnect() {
@@ -192,7 +204,7 @@ void loop() {
   server.handleClient();
 
   CheckSerial();
-  CheckMqtt();
+  //CheckMqtt();
 }
 
 void CheckMqtt() {
@@ -219,24 +231,26 @@ void serialEvent() {
     size_t len = Serial.available();
     uint8_t sbuf[len];
     Serial.readBytes(sbuf, len);
-    String inputString="";
-    for(int i=0;i<len;i++)
-      inputString += char(sbuf[i]);
-    Serial.println(inputString);
-    if( inputString.startsWith(":800404") == true) {
+    for(int i=0;i<len;i++) {
+      if(char(sbuf[i]) == ':') 
+        inputString = "";
+     inputString += char(sbuf[i]);
+    }
+    //Serial.println(inputString);
+    if( inputString.length() >= 15) {
       int Humi, Temp;
       Serial.println("OK");
-      Humi = HexToInt(sbuf[7])*16*16*16 + HexToInt(sbuf[8])*16*16 + 
-      HexToInt(sbuf[9])*16 + HexToInt(sbuf[10]);
+      Humi = HexToInt(inputString.charAt(7))*16*16*16 + HexToInt(inputString.charAt(8))*16*16 + 
+             HexToInt(inputString.charAt(9))*16 + HexToInt(inputString.charAt(10));
       humidity = (float)Humi/100;
       Serial.println(humidity);
-      Temp = HexToInt(sbuf[11])*16*16*16 + HexToInt(sbuf[12])*16*16 + 
-      HexToInt(sbuf[13])*16 + HexToInt(sbuf[14]);
+      Temp = HexToInt(inputString.charAt(11))*16*16*16 + HexToInt(inputString.charAt(12))*16*16 + 
+             HexToInt(inputString.charAt(13))*16 + HexToInt(inputString.charAt(14));
       temperature = (float)Temp/100;
       Serial.println(temperature);
       // 온도와 습도 값을 브로커에 보낸다.
       sprintf (msg, "\{Humidity: %f, Temperature: %f\}", humidity, temperature);
-      client.publish(outTopic, msg);
+      //client.publish(outTopic, msg);
     }
   }
 }
--- 3-3-5_온습도센서(MHTP-485S) MQTT 한글/3-3-5_mhtp485/handleHttp.ino
+++ 3-3-5_온습도센서(MHTP-485S) MQTT 한글/3-3-5_mhtp485/handleHttp.ino
@@ -4,16 +4,26 @@ void handleRoot() {
   Serial.println ( WiFi.localIP() );
   String s; 
   String s1= String(ssid);
-  s="<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\", http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\"/>";
+  s="<meta name='viewport' content='width=device-width, initial-scale=1.0'/>";
+  s=s+"<meta http-equiv='refresh' content='5'/>";
+  s=s+"<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />";
   s=s+"<h1>온도 "+temperature+" ℃<br>"+"습도 "+humidity+" %</h1><br>";
-  s=s+"Connected IP : "+String(ssid)+"  "+WiFi.localIP().toString()+"<p><a href='/wifi'>공유기를 바꾸려면 누르세요.</a></p>";
+  
+  if(setAP==1) {
+    s=s+"아래 공유기 이름과 주소가 연결되었으면 주소를 선택한 후에 설정에서 사용 하시는 인터넷 공유기를 선택하세요. <br>";
+    s=s+"Connected IP : "+String(ssid)+"  "+"<p><a href='http://"+WiFi.localIP().toString()+"'/>"+WiFi.localIP().toString()+"</a></p>";
+  }
+  s=s+"<p><a href='/wifi'>공유기를 바꾸려면 누르세요.</a></p>";
+  setAP=0;
   server.send(200, "text/html", s); // Empty content inhibits Content-length header so we have to close the socket ourselves.
 }
 
 void handleWifi() {
   String s; 
   String s1= String(ssid);
-  s="<meta name=\"viewport\" content=\"width=device-width, user-scalable=no\", meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\"/>";
+  //s="<meta name=\"viewport\" content=\"width=device-width, user-scalable=no\", meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\", meta http-equiv='refresh' content='5'/>";
+  s="<meta name=\"viewport\" content=\"width=device-width, user-scalable=no\", meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />";
+  //s=s+"<meta http-equiv='refresh' content='5'/>";
   s=s+"<h1>Wifi 사양</h1>";
   if (server.client().localIP() == apIP) {
     Serial.println(String(softAP_ssid));
@@ -48,6 +58,7 @@ void handleWifi() {
     +"<br /><input type='submit' value='      저    장      '/></form>"
     +"<p><a href='/'>메인 홈페이지로 가기</a>.</p>";
   server.send(200, "text/html", s);
+  setAP=1;
 }
 
 /** Handle the WLAN save form and redirect to WLAN config page again */
