6eaca4aa497506676bb8abe2e72974a72e7b0c04
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2019-05-06 14:13:45 +0900

    RGB Led Control
    빨강 초록 파랑색 신호를 PWM을 이용하여 Led 밝기와 색상을 조절한다.

---
+++ 3-0-6 RGB Led/RGBLed/RGBLed.ino
@@ -0,0 +1,31 @@
+int d1=D1;
+int d2=D2;
+int d3=D3;
+void setup()
+{
+ pinMode(d1, OUTPUT);
+ pinMode(d2, OUTPUT);
+ pinMode(d3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(d1,255);  //Red
+  digitalWrite(d2,0);   //Green
+  digitalWrite(d3,0);   //Blue
+  delay(1000);           
+  digitalWrite(d1,0);  
+  digitalWrite(d2,255);  
+  digitalWrite(d3,0);   
+  delay(1000);
+  digitalWrite(d1,0);  
+  digitalWrite(d2,0);  
+  digitalWrite(d3,255); 
+  delay(1000);
+  digitalWrite(d1,255);  
+  digitalWrite(d2,255);  
+  digitalWrite(d3,255); 
+  delay(1000);
+}
+
+
