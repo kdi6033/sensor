8f3cb3a452c603b1532f722ee53938c428a3b80d
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2020-01-09 16:00:17 +0900

    metering pump

---
+++ 3-21 MeteringPumpDc/mqtt_json/mqtt_json.ino
@@ -0,0 +1,127 @@
+//mqtt 참조 1-16-3 https://youtu.be/6rfDrpsOGMY 
+//보드제어참조 1-10 https://youtu.be/0Xlp7dUzgiU
+//보드구매사이트 https://smartstore.naver.com/i2r/products/574514515
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+// 아래의 6개설정은 사용자 환경에 맞게 수정하세요.
+const char* ssid = "i2r"; // 와이파이 AP, 또는 스마트폰의 핫스판 이름
+const char* password = "00000000";  // 와이파이 AP, 또는 스마트폰의 핫스판 이름
+const char* mqtt_server = "broker.mqtt-dashboard.com"; //브로커 주소
+const char* outTopic = "/pump/outTopic"; // 이름이 중복되지 않게 설정 기록
+const char* inTopic = "/pump/inTopic"; // 이름이 중복되지 않게 설정 기록
+const char* clientName = "";  // setup 함수에서 자동생성
+String sChipID;
+char cChipID[20];
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+
+//json을 위한 설정
+StaticJsonDocument<200> doc;
+DeserializationError error;
+JsonObject root;
+
+//펌모터 제어 변수
+const int led1Pin =  4;
+boolean bLED1 = false; 
+const int led2Pin =  0;
+boolean bLED2 = false; 
+
+void setup() {
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  digitalWrite(led1Pin, HIGH);
+  digitalWrite(led2Pin, HIGH); 
+  
+  Serial.begin(9600);
+  setup_wifi();
+
+  //이름 자동으로 생성
+  sChipID=String(ESP.getChipId(),HEX);
+  sChipID.toCharArray(cChipID,sChipID.length()+1);
+  clientName=&cChipID[0];
+  Serial.println(clientName);
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// 통신에서 문자가 들어오면 이 함수의 payload 배열에 저장된다.
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  deserializeJson(doc,payload);
+  root = doc.as<JsonObject>();
+  //const char* red = root["r"];
+  int rot=root["rot"];
+  rotate(rot);
+}
+
+// mqtt 통신에 지속적으로 접속한다.
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientName)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(outTopic, "Reconnected");
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+}
+
+void rotate(int mSec) {
+  Serial.println("start");
+  Serial.println(mSec);
+  digitalWrite(led2Pin, LOW);
+  delay(mSec);
+  digitalWrite(led2Pin, HIGH);
+}
---
+++ 3-21 MeteringPumpDc/pump_dc/pump_dc.ino
@@ -0,0 +1,29 @@
+
+#include <ESP8266WiFi.h>
+
+const char* ssid = "i2r";
+const char* password = "00000000";
+const int led1Pin =  4;
+boolean bLED1 = false; 
+const int led2Pin =  0;
+boolean bLED2 = false; 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  digitalWrite(led1Pin, HIGH);
+  digitalWrite(led2Pin, HIGH);  
+  
+  Serial.begin(9600);
+  delay(10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("start");
+  digitalWrite(led2Pin, LOW);
+  delay(1500);
+  digitalWrite(led2Pin, HIGH);
+  delay(3000);
+}
