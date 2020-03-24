1167e28e00710db296affed712f80499b53b1b1a
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2020-03-24 17:38:13 +0900

    lora

---
+++ 3-24-3 Loadcell HX711 LoRa+Deep Sleep/LoRa_DeepSleep/LoRa_DeepSleep.ino
@@ -0,0 +1,107 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+#include <Q2HX711.h>
+
+
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  10        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
+const byte hx711_data_pin = 12;
+const byte hx711_clock_pin = 13;
+Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISnO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND  923E6
+
+unsigned int count = 0;
+long reading;
+uint64_t chipid;
+String chipidS;
+
+SSD1306 display(0x3c, 21, 22);
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println();
+  Serial.println("LoRa Sender");
+
+  chipid=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+  //Serial.printf("ESP32 Chip ID = %04X",(uint16_t)(chipid>>32));//print High 2 bytes
+  //Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
+  chipidS=String(((uint16_t)(chipid>>32)),HEX)+String(((uint32_t)chipid),HEX);
+  Serial.println(chipidS);
+
+  //WakeUp
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+  /*
+  First we configure the wake up source
+  We set our ESP32 to wake up every 5 seconds
+  */
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+
+  //loRa
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(923E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("init ok");
+}
+
+void loop() {
+  //drawFontFaceDemo();
+
+  reading=(hx711.read()-16694000)/(-5000)*100;
+  //reading=hx711.read();
+  Serial.println(reading);
+  // send packet
+  //LoRa.beginPacket();
+  //LoRa.print("{\"chip\":\"");
+  //LoRa.print(chipidS);
+  //LoRa.print("\",\"weight\":");
+  LoRa.print(reading);
+  LoRa.print("}");
+  LoRa.endPacket();
+  count++;
+  Serial.println(count);
+  delay(2000);
+  if(count>5)
+    esp_deep_sleep_start();                      
+}
+
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
