0f6b5b4d3e705ee73d5369c8a7fd443f7f04b399
kdi6033 <kdi6033@gmail.com>
2020-11-11 16:29:27 +0900

    Create RelayShield.ino

---
+++ 3-0-3 Relay/RelayShield/RelayShield.ino
@@ -0,0 +1,14 @@
+int d2=D1;
+void setup()
+{
+ Serial.begin(115200); // 시리얼 통신 초기화
+ pinMode(d2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(d2,HIGH);
+  delay(1000);
+  digitalWrite(d2,LOW);
+  delay(1000);
+}
